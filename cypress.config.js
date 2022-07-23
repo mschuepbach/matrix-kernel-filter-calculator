0aa2b23a943ce8d9f62987ccba5df2542fdef228
mschuepbach <schuepbach.matteo@gmail.com>
2022-07-23 18:11:39 +0200

    Turn off screenshots and videos

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080/",
+    screenshotOnRunFailure: false,
+    video: false,
   },
 });
